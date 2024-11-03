0c7c1532663d49255b1a023ceefe80a257d7db64
di <diego@apicesistemas.com.br>
2024-11-03 11:58:02 -0300

    Dando inicio ao projeto
    - primeiras configurações e pacotes
    - inicio da tela de login

---
+++ financial-management-app/frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import "./Login.css"
+
+function Login() {
+  return (
+    <div className="main">
+        <div className="boxLogin">
+                  </div>
+    </div>
+  );
+}
+
+export default Login;
